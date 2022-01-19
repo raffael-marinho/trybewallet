cd18f0675e6c5bd4afc7dc0dee52f78304d1ff3c
raffael marinho <silvamarinho10@gmail.com>
2022-01-19 00:56:37 -0300

    feat: requirement 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function Wallet(prop) {
+  const { email } = prop;
   return (
     <header>
-      <h1 data-testid="email-field">{prop.email}</h1>
+      <h1 data-testid="email-field">{email}</h1>
+      <h2 data-testid="total-field">0</h2>
+      <h2 data-testid="header-currency-field">BRL</h2>
     </header>
   );
 }
