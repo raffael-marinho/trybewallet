63e24c5482a036396f1aaeac65ab8735cf9b7deb
raffael marinho <silvamarinho10@gmail.com>
2022-01-26 22:29:28 -0300

    feat:requirement 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormWallet from '../components/FormWallet';
 import WalletHeader from '../components/WalletHeader';
+import Tabela from '../components/Tabela';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <WalletHeader />
         <FormWallet />
+        <Tabela />
       </div>
     );
   }
